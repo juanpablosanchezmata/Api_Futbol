def270e2ed4764eb4d3595323f161dffc4bae1d3
juanpablosanchezmata <111403782+juanpablosanchezmata@users.noreply.github.com>
2025-11-17 15:29:45 -0600

    commit final

--- app.py
+++ app.py
@@ -149,4 +149,4 @@ def delete_team(id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5001)), debug=True)
